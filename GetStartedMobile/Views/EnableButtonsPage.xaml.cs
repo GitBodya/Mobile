e031cdcbaf6e2313a541609ac7d03de52a811080
Bohdanluzhetskyi <bluzhetskyi@netfully.com>
2024-02-01 15:06:18 +0200

    fix: delete unused views like BetterColorS., EnableButtonsP.., SwitchIndicatorP...

--- GetStartedMobile/Views/EnableButtonsPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace GetStartedMobile.Views;
-
-public partial class EnableButtonsPage : ContentPage
-{
-	public EnableButtonsPage()
-	{
-		InitializeComponent();
-	}
-}
--- GetStartedMobile/Views/SwitchIndicatorsPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace GetStartedMobile.Views;
-
-public partial class SwitchIndicatorsPage : ContentPage
-{
-	public SwitchIndicatorsPage()
-	{
-		InitializeComponent();
-	}
-}
